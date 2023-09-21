7b3ec6729f071288f8cb68c5888399afe972788f
dow <dowabc@outlook.com>
2023-09-21 12:39:04 +0800

    page

--- app/(auth)/admin/photo/page.tsx
+++ app/(auth)/admin/photo/page.tsx
@@ -17,24 +17,24 @@ import { log } from 'console';
 
 async function Photo() {
     // const initialData = await getPhotos();
-   //            const list = await request.get(`/api/index`, { currentPage: 1 });
+    //            const list = await request.get(`/api/index`, { currentPage: 1 });
 
-//    const photo = await request.get(`https://www.bobjoy.com/api/index?currentPage=1`);
-    // const res = await getPhotos2();
+    //    const photo = await request.get(`https://www.bobjoy.com/api/index?currentPage=1`);
+    const res = await getPhotos2();
 
-    // console.log('res:',res);
-    // // const photo:Photo[]= res.data
+    console.log('res:', res);
+    // const photo:Photo[]= res.data
 
-    // const queryClient = getQueryClient();
-    // await queryClient.prefetchQuery(['getp'],res.data.data)
-    
+    const queryClient = getQueryClient();
+    await queryClient.prefetchQuery(['getp'], res.data.data)
+
 
     return (
         <Box>
             <PhotoToolbar />
             {/* <PhotoItem list={initialData} /> */}
             <Grid container spacing={5} style={{ marginTop: 0, marginBottom: 45 }}>
-                    <PhotoCard/>
+                <PhotoCard />
             </Grid>
         </Box>
     );
